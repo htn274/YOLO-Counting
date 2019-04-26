c0a371cce10b885240cdd93f211c03de919ec61c
exp-technology <45615867+exp-technology@users.noreply.github.com>
2019-04-26 15:49:28 +0700

    Add files via upload

---
+++ jalan.sh
@@ -0,0 +1,2 @@
+./darknet detector demo cfg/coco.data cfg/yolov3-tiny_obj.cfg yolov3-tiny.weights rtsp://root:pass@192.168.0.177/axis-media/media.amp
+
---
+++ jalan_zed.sh
@@ -0,0 +1,2 @@
+./darknet detector demo cfg/coco.data cfg/yolov3-tiny_obj.cfg yolov3-tiny.weights -c 1
+
